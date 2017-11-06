1648eca242f6e320eb25b82871f4db35e25c7331
penalzajohn25 <33187074+penalzajohn25@users.noreply.github.com>
2017-11-06 10:38:21 -0400

    actualizar codigos

--- app/view/GrillaViewport.js
+++ app/view/GrillaViewport.js
@@ -72,57 +72,7 @@ Ext.define('AM.view.GrillaViewport', {
                                     return;
                                 }
                             },
-                            {
-                                text: 'Ver todo',
-                                handler: function() {
-                                    var form = this.up('form').getForm();
-                                    form.reset();
-                                    if (form.isValid()) {
-                                        form.submit({
-                                            method: 'POST',
-                                            url: 'http://grupoinelecpro.com.ve/Alegra/public/application/cliente/search',
-                                            waitTitle: "Por favor espere",
-                                            waitMsg: 'Enviando datos  ...',
-                                            success: function(form, action) {
-                                                var msg = Ext.Msg.show({
-                                                    title: 'Exito',
-                                                    msg: 'Iniciando busqueda',
-                                                    buttons: Ext.Msg.OK,
-                                                    fn: function(key) {
-                                                        form.reset();
-                                                        var grid = me.down("gridpanel");
-                                                        grid.getStore().load();
-                                                        grid.getView().refresh();
-                                                        me.show();
-                                                        return;
-                                                    },
-                                                    icon: Ext.MessageBox.INFO
-                                                });
-                                                setTimeout(
-                                                    function() {
-                                                        msg.hide();
-                                                        form.reset();
-                                                        var grid = me.down("gridpanel");
-                                                        grid.getStore().load();
-                                                        grid.getView().refresh();
-                                                        me.show();
-                                                        return;
-                                                    },
-                                                    300
-                                                );
-                                                return;
-                                            },
-                                            failure: function(form, action) {
-                                                Ext.Msg.alert('Fallo', 'Hay un error en el envio de datos');
-                                                return;
-                                            }
-                                        });
-                                    } else {
-                                        Ext.Msg.alert('Fallo', 'Falta llenar datos en el formulario');
-                                    }
-                                    return;
-                                }
-                            },
+
                             {
                                 text: 'Nuevo',
                                 handler: function() {
@@ -130,32 +80,7 @@ Ext.define('AM.view.GrillaViewport', {
                                     usuario.show();
                                 }
                             },
-                            {
-                                text: 'Borrar',
-                                handler: function() {
-                                    Ext.Ajax.request({
-                                        url: 'usuario/delete',
-                                        method: 'post',
-                                        params: {
 
-                                        },
-                                        success: function(response, opts) {
-                                            var responseData = null;
-                                            try { responseData = response['responseText']; } catch (ex) { responseData = ''; }
-                                            var data = null;
-                                            try { data = Ext.decode(responseData); } catch (ex) {}
-                                            if (data != null) {
-                                                Ext.Msg.alert("Mensaje", "Datos borrados");
-                                                me.down("gridpanel").getStore().load();
-                                                me.down("gridpanel").getView().refresh();
-                                            } else {
-                                                Ext.Msg.alert("Error", "Hubo un error en el proceso");
-                                            }
-                                            return;
-                                        }
-                                    });
-                                }
-                            }
                         ],
                         frame: true,
                         layout: {
@@ -164,20 +89,12 @@ Ext.define('AM.view.GrillaViewport', {
                         bodyPadding: 10,
                         title: '',
                         items: [{
-                                xtype: 'textfield',
-                                columnWidth: 0.5,
-                                padding: 5,
-                                fieldLabel: 'Nombre',
-                                name: 'nombre'
-                            },
-                            {
-                                xtype: 'textfield',
-                                columnWidth: 0.5,
-                                padding: 5,
-                                fieldLabel: 'Apellidos',
-                                name: 'apellidos'
-                            }
-                        ]
+                            xtype: 'textfield',
+                            columnWidth: 0.5,
+                            padding: 5,
+                            fieldLabel: 'Nombre',
+                            name: 'nombre'
+                        }]
                     }]
                 },
                 {
@@ -217,31 +134,7 @@ Ext.define('AM.view.GrillaViewport', {
                             flex: 1
                         }
                     ],
-                    listeners: {
-                        'cellclick': function(grid, td, cellIndex, record, tr, rowIndex, event, object) {
-                            var id = record.get('id');
-                            Ext.Ajax.request({
-                                url: 'usuario/select',
-                                method: 'post',
-                                params: {
-                                    'id': id
-                                },
-                                success: function(response, opts) {
-                                    var responseData = null;
-                                    try { responseData = response['responseText']; } catch (ex) { responseData = ''; }
-                                    var data = null;
-                                    try { data = Ext.decode(responseData); } catch (ex) {}
-                                    if (data != null) {
 
-                                    } else {
-                                        Ext.Msg.alert("Error", "Hubo un error en el proceso");
-                                    }
-                                    return;
-                                }
-                            });
-                            return;
-                        }
-                    },
                     store: Ext.create(
                         'Ext.data.Store', {
                             remoteSort: true,
